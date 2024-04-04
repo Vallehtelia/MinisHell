e31ec10bbf9bcf6e75b81739f646ce23cc711c9c
MRinkinen <101101773+MRinkinen@users.noreply.github.com>
2024-04-04 13:15:25 +0300

    hardcode function

--- sources/minishell.c
+++ sources/minishell.c
@@ -14,6 +14,24 @@
 
 int	global_signal = 0;
 
+int hardcode(t_minishell *mshell, char *input)
+{
+	if (ft_strncmp(input, "exit", ft_strlen(input) + 1) == 0)
+	{
+		free(input);
+		exit_and_free(mshell, 0);
+	}
+	if (ft_strncmp(input, "$?", 3) == 0)
+	{
+		add_history(input);
+		free(input);
+		printf("minisHell: %d: command not found\n", global_signal);
+		global_signal = 127;
+		return (1);
+	}
+	return (0);
+}
+
 int	main(int ac, char **av, char **envp)
 {
 	char		*input;
@@ -31,11 +49,8 @@ int	main(int ac, char **av, char **envp)
 		if (!input)
 			break ;
 		free_workingdir(&mshell);
-		if (ft_strncmp(input, "exit", 5) == 0)
-		{
-			free(input);
-			exit_and_free(&mshell, 0);
-		}
+		if(hardcode(&mshell, input))
+			continue ;
 		mshell.input_cmd = input;
 		matti(&mshell);
 		valle(&mshell);
